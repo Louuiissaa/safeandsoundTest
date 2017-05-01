2b9c1733fa048e37895766ea3c59bc3cff95c47b
Luisa Papst <lpabst@hotmail.de>
2017-05-01 12:01:00 +0200

    First Commit

--- app/src/main/java/com/safeandsound/app/safeandsound/controller/MainActivity.java
+++ app/src/main/java/com/safeandsound/app/safeandsound/controller/MainActivity.java
@@ -1,7 +1,9 @@
-package com.safeandsound.app.safeandsound;
+package com.safeandsound.app.safeandsound.controller;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.safeandsound.app.safeandsound.R;
 
 public class MainActivity extends AppCompatActivity {
 
